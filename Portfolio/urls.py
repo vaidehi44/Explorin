936debca18b298df30d467651d5e872b2642b982
vaidehi44 <vaidehiatpadkar1@gmail.com>
2021-01-28 18:29:25 +0530

    Responsive design

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -10,6 +10,13 @@ urlpatterns = [
     path('about',views.aboutView, name='about'),
     path('projects',views.projectView, name='projects'),
     path('contact',views.contactView, name='contact'),
+    path('tasks/api', views.apiView, name="api"),
+    path('tasks/calculator', views.calculatorView, name="calculator"),
+    path('tasks/flexbox', views.flexboxView, name="flexbox"),
+    path('tasks/sql', views.sqlView, name="sql"),
+    path('tasks/website', views.websiteView, name="website"),
+    path('tasks/flexbox_advanced', views.flex_advView, name="flex_adv"),
+    path('tasks/login_page', views.loginView, name="login_page"),
 
 
 ]
--- Portfolio/views.py
+++ Portfolio/views.py
@@ -12,3 +12,24 @@ def projectView(request):
 
 def contactView(request):
     return render(request, "portfolio/contact.html")
+
+def apiView(request):
+    return render(request, "portfolio/tasks/api.html")
+
+def calculatorView(request):
+    return render(request, "portfolio/tasks/calculator.html")
+
+def flexboxView(request):
+    return render(request, "portfolio/tasks/flexbox.html")
+
+def sqlView(request):
+    return render(request, "portfolio/tasks/sql.html")
+
+def websiteView(request):
+    return render(request, "portfolio/tasks/website.html")
+
+def flex_advView(request):
+    return render(request, "portfolio/tasks/flex_adv.html")
+
+def loginView(request):
+    return render(request, "portfolio/tasks/login.html")
